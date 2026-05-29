980cd977644251d7a0330127bd664e35aeffefcc
MrAghanims <168049850+MrAghanims@users.noreply.github.com>
2026-05-30 00:17:45 +0800

    Trial

---
+++ Sa Ilalim ng Puting Ilaw/Assets/Scripts/Manager/Next.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Next : MonoBehaviour
+{
+    public string Nextscene;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        
+     
+    }
+
+    public void GoNext()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(Nextscene);
+    }
+
+}
